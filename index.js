74dc458d3995de90ec9bf1603f6eac39042cb2c6
Jocelyn <jocelynvargas123@yahoo.com>
2023-04-25 01:05:44 -0700

    lab not done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+        name : name,
+        email : email,
+    }
+
+    const dataObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json',
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch('http://localhost:3000/users', dataObject)
+    .then((response) => response.json())
+
+    .then(function (data) {
+        const newDiv = document.createElement('div')
+        document.querySelector('body').appendChild(newDiv)
+        document.querySelector('div').innerHTML = data.id
+    })
+    .catch((e) => {
+        return console.log("error")
+
+    })
+}
